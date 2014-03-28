9484155af757902d5b35f7dfce87663c160dec86
Haim Tzabar <haimtzabar@gmail.com>
2014-03-28 10:40:02 +0300

    Add message

---
+++ Moshavit.Entity/EntityTable/Message.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Moshavit.Entity.EntityTable
+{
+    public abstract class  Message
+    {
+        public int Id { get; set; }
+        public int IdUser { get; set; }
+        public string Title { get; set; }
+        public string Subject { get; set; }
+        public string Content { get; set; }
+    }
+}
