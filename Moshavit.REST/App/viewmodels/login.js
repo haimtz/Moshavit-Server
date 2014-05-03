0b0a2fcc7afa64671d6f493fbf2be6014f55a36e
Haim Tzabar <haimtzabar@gmail.com>
2014-05-03 10:59:28 +0300

    Add Durandal and less

---
+++ Moshavit.REST/App/viewmodels/login.js
@@ -0,0 +1,15 @@
+﻿define(function() {
+
+    function activate(){
+    }
+
+    function compositionComplete() {
+        
+    }
+
+    var viewModel = {
+        activate: activate,
+        compositionComplete: compositionComplete
+    };
+    return viewModel;
+});
--- Moshavit.REST/Global.asax.cs
+++ Moshavit.REST/Global.asax.cs
@@ -4,7 +4,9 @@ using System.Linq;
 using System.Web;
 using System.Web.Http;
 using System.Web.Mvc;
+using System.Web.Optimization;
 using System.Web.Routing;
+using Moshavit.REST.App_Start;
 
 namespace Moshavit.REST
 {
@@ -19,6 +21,7 @@ namespace Moshavit.REST
             WebApiConfig.Register(GlobalConfiguration.Configuration);
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundels(BundleTable.Bundles);
 
             Bootstrap.Initialized();
         }
