e183d291455260e507b16c2dba91187ae2c3968b
haimtz <haimtzabar@gmail.com>
2014-10-15 01:43:25 +0300

    Mail const

---
+++ Moshavit.Const/MailConst.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Moshavit.Const
+{
+    public class MailConst
+    {
+        public class From
+        {
+            public const string EMAIL = "DoNotReplay@moshavit.com";
+            public const string DO_NOT_REPLAY = "Do Not Replay";
+        }
+
+        public class Account
+        {
+            public const string HOST = "smtp.gmail.com";
+            public const string MAIL_ACCOUNT = "***********";
+            public const string PASSWORD = "*****************";
+            public const int PORT = 587;
+        }
+
+        public class Template
+        {
+            public const string FORGOT_PASSWORD = "Forgot Password";
+        }
+    }
+}
---
+++ Moshavit.Settings/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+using Moshavit.Castel;
+using Moshavit.Const;
+using Moshavit.Entity;
+using Moshavit.Entity.Interfaces;
+using Moshavit.MailService;
+
+namespace Moshavit.Settings
+{
+    class Program
+    {
+        private static IDependency _resolver;
+        static void Main(string[] args)
+        {
+            _resolver = new CastleFactory();
+            LoadTemplateMail();
+        }
+
+        private static void LoadTemplateMail()
+        {
+            var path = Path.Combine(Environment.CurrentDirectory, "forgotmail.html");
+            var content = File.ReadAllText(path);
+
+            var emailTemplate = new EmailTemplate
+            {
+                Name = MailConst.Template.FORGOT_PASSWORD,
+                Content = content
+            };
+
+            var service = _resolver.Resolver<ITemplateService>();
+            
+            service.AddTempalte(emailTemplate);
+        }
+    }
+}
